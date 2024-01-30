c3b432678c262e7b5ad9443b4c22f7304ab3b64c
tamimhasan19702 <tamimhasancu@gmail.com>
2024-01-30 20:20:53 +0600

    sendToSpeak UpdateS

--- src/features/sendToSpeaker/sendToSpeaker.js
+++ src/features/sendToSpeaker/sendToSpeaker.js
@@ -1,12 +1,13 @@
 /** @format */
 
-import { View, Text, TouchableOpacity, Button } from "react-native";
+import { View, Text, TouchableOpacity, Button, StyleSheet } from "react-native";
 import React, { useState, useEffect, useContext } from "react";
 import { SafeView } from "../../utils/safeAreaView";
 import styled from "styled-components";
 import { LogoBar } from "../../components/logoBar.component";
 import { AiContext } from "../../context/AI.context";
 import { SpeakerContext } from "../../context/Speaker.context";
+import { FIREBASEDATABASE } from "../../../firebase.config";
 
 const SendToSpeakerView = styled(View)`
   display: flex;
@@ -16,18 +17,47 @@ const SendToSpeakerView = styled(View)`
 `;
 
 export const SendToSpeaker = ({ navigation }) => {
+  const [pressed, setPressed] = useState(false);
   const { speakers } = useContext(SpeakerContext);
-  console.log(speakers);
+
+  const onPress = () => {
+    console.log(speakers);
+  };
+
+  const onLongPress = () => {
+    setPressed(true);
+    // Add loading animation logic here
+  };
+
   return (
     <SafeView>
       <LogoBar link={navigation} icon={"arrow-left"} route={"Speaker Screen"} />
       <SendToSpeakerView>
-        <Text>{speakers[0].text}</Text>
-        <Text>{speakers[1].text}</Text>
-        <Text>{speakers[2].text}</Text>
-        <Text>{speakers[3].text}</Text>
-        <Text>{speakers[4].text}</Text>
+        {speakers.map(
+          (speaker, index) =>
+            speaker.text && <Text key={index}>{speaker.text}</Text>
+        )}
+        <TouchableOpacity
+          onPress={onPress}
+          onLongPress={onLongPress}
+          onPressOut={() => setPressed(false)}>
+          <View
+            style={[
+              styles.button,
+              { backgroundColor: pressed ? "blue" : "gray" },
+            ]}>
+            <Text>Press and hold me</Text>
+          </View>
+        </TouchableOpacity>
       </SendToSpeakerView>
     </SafeView>
   );
 };
+
+const styles = StyleSheet.create({
+  button: {
+    padding: 10,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
