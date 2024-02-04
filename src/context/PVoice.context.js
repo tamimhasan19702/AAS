06946bc31ea3914a30852af81323f66da7f6b370
tamimhasan19702 <tamimhasancu@gmail.com>
2024-02-05 00:52:35 +0600

    successfully stored my voice in a state to get the recorded voice whenever i neede

--- src/context/PVoice.context.js
+++ src/context/PVoice.context.js
@@ -5,7 +5,73 @@ import { Audio } from "expo-av";
 export const PVoiceContext = createContext();
 
 export const PVoiceContextProvider = ({ children }) => {
-  const contextValue = {};
+  const [recording, setRecording] = useState();
+  const [sound, setSound] = useState();
+  const [myRecording, setMyRecording] = useState();
+
+  async function startRecording() {
+    try {
+      const { status } = await Audio.requestPermissionsAsync();
+      if (status !== "granted") {
+        console.log("Requesting permission..");
+        return;
+      }
+      await Audio.setAudioModeAsync({
+        allowsRecordingIOS: true,
+        playsInSilentModeIOS: true,
+      });
+
+      console.log("Starting recording..");
+      const recording = new Audio.Recording();
+      await recording.prepareToRecordAsync(
+        Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY
+      );
+      await recording.startAsync();
+      console.log(recording);
+      setRecording(recording);
+
+      console.log("Recording started");
+    } catch (err) {
+      console.error("Failed to start recording", err);
+    }
+  }
+
+  async function stopRecording() {
+    try {
+      console.log("Stopping recording..");
+      await recording.stopAndUnloadAsync();
+      await Audio.setAudioModeAsync({
+        allowsRecordingIOS: false,
+      });
+      const { sound } = await recording.createNewLoadedSoundAsync();
+      setSound(sound);
+      setRecording(false);
+      setMyRecording(sound); // Save the sound in myRecording state
+      console.log("Recording stopped and sound created");
+    } catch (err) {
+      console.error("Failed to stop recording", err);
+    }
+  }
+
+  async function playRecording() {
+    try {
+      if (myRecording) {
+        console.log("Playing recording..");
+        await myRecording.replayAsync(); // Replay the sound
+        console.log("Recording playing");
+      }
+    } catch (err) {
+      console.error("Error playing recording", err);
+    }
+  }
+  const contextValue = {
+    myRecording,
+    recording,
+    sound,
+    startRecording,
+    stopRecording,
+    playRecording,
+  };
 
   return (
     <PVoiceContext.Provider value={{ ...contextValue }}>
--- src/features/voice/Voice.feature.js
+++ src/features/voice/Voice.feature.js
@@ -6,7 +6,6 @@ import { SafeView } from "../../utils/safeAreaView";
 import styled from "styled-components";
 import { LogoBar } from "../../components/logoBar.component";
 import { PVoiceContext } from "../../context/PVoice.context";
-import { Audio } from "expo-av";
 
 const VoiceScreenView = styled(View)`
   display: flex;
@@ -16,50 +15,8 @@ const VoiceScreenView = styled(View)`
 `;
 
 export const VoiceScreen = ({ navigation }) => {
-  const [recording, setRecording] = useState();
-  const [sound, setSound] = useState();
-
-  async function startRecording() {
-    try {
-      const { status } = await Audio.requestPermissionsAsync();
-      if (status !== "granted") {
-        console.log("Requesting permission..");
-        return;
-      }
-      await Audio.setAudioModeAsync({
-        allowsRecordingIOS: true,
-        playsInSilentModeIOS: true,
-      });
-
-      console.log("Starting recording..");
-      const recording = new Audio.Recording();
-      await recording.prepareToRecordAsync(
-        Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY
-      );
-      await recording.startAsync();
-      setRecording(recording);
-      console.log("Recording started");
-    } catch (err) {
-      console.error("Failed to start recording", err);
-    }
-  }
-
-  async function stopRecording() {
-    console.log("Stopping recording..");
-    await recording.stopAndUnloadAsync();
-    await Audio.setAudioModeAsync({
-      allowsRecordingIOS: false,
-    });
-    const { sound } = await recording.createNewLoadedSoundAsync();
-    setSound(sound);
-    console.log("Recording stopped and sound created");
-  }
-
-  async function playRecording() {
-    console.log("Playing recording..");
-    await sound.playAsync();
-    console.log("Recording playing");
-  }
+  const { recording, sound, startRecording, stopRecording, playRecording } =
+    useContext(PVoiceContext);
 
   return (
     <SafeView>
