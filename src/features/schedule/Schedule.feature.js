c52a377f0f572789584b5c55f644364761ec5d62
tamimhasan197020 <tamimhasancu@gmail.com>
2024-03-18 17:24:59 +0600

    update Schedule.feature.js

--- src/features/schedule/Schedule.feature.js
+++ src/features/schedule/Schedule.feature.js
@@ -5,19 +5,53 @@ import React from "react";
 import styled from "styled-components";
 import { SafeView } from "../../utils/safeAreaView";
 import { LogoBar } from "../../components/logoBar.component";
+import { color } from "../../utils/colors";
+import { TextInput } from "react-native-paper";
 
 const ScheduleView = styled(View)`
   display: flex;
-  justify-content: center;
+  justify-content: start;
   align-items: center;
   height: 100%;
 `;
+
+const ScheduleText = styled(Text)`
+  font-weight: 400;
+  font-size: 25px;
+  text-align: center;
+  padding: 10px 0px;
+  font-family: "OverlockSC_400Regular";
+`;
+
+const ScheduleInputView = styled(View)`
+  width: 80%;
+`;
+
+const ScheduleInputField = styled(TextInput)`
+  width: 100%;
+  margin: 10px 0px;
+  padding: 10px 0px;
+  text-vertical-align: top;
+`;
+
 export const ScheduleScreen = ({ navigation }) => {
   return (
     <SafeView>
       <LogoBar link={navigation} icon={"arrow-left"} />
       <ScheduleView>
-        <Text>ScheduleScreen</Text>
+        <ScheduleText>Schedule a Annoucnements</ScheduleText>
+        <ScheduleInputView>
+          <ScheduleInputField
+            placeholder="Please Enter Text Here"
+            onChangeText={() => {}}
+            underlineColor={color.primary}
+            mode="outlined"
+            autoFocus={false}
+            autoCapitalize="none"
+            autoCorrect={false}
+            multiline={true}
+          />
+        </ScheduleInputView>
       </ScheduleView>
     </SafeView>
   );
