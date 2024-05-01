9fad0c3bd5b8e4c668396173ba65f2d68635b374
tamimhasan197020 <tamimhasancu@gmail.com>
2024-05-01 19:44:52 +0600

    update app.json, src/components/Navigation/index.js and splash.feature.js

--- src/features/splash/splash.feature.js
+++ src/features/splash/splash.feature.js
@@ -4,6 +4,8 @@ import React, { useState, useEffect } from "react";
 import { SafeView } from "../../utils/safeAreaView";
 import LottieView from "lottie-react-native";
 import styled from "styled-components";
+import { Loading } from "../../utils/loading";
+import { Image, Text } from "react-native";
 
 const SplashView = styled(SafeView)`
   display: flex;
@@ -12,27 +14,21 @@ const SplashView = styled(SafeView)`
   height: 100vh;
 `;
 
+const LottieViewSplash = styled(LottieView)`
+  width: 70%;
+  height: 70%;
+`;
+
 export const SplashScreen = ({ setIsLoading }) => {
-  // useEffect(() => {
-  //   const timeout = setTimeout(() => {
-  //     setIsLoading(false);
-  //   }, 2800);
-
-  //   return () => clearTimeout(timeout);
-  // }, [setIsLoading]);
-
   return (
     <SplashView>
-      <LottieView
+      <LottieViewSplash
         autoPlay
-        autoSize
         loop={false}
         source={{
           uri: "https://lottie.host/ee777ab4-b025-4395-a401-ab09cfb5043e/5ytIYi3chy.json",
         }}
-        onAnimationFinish={() => {
-          setIsLoading(false);
-        }}
+        onAnimationFinish={() => setIsLoading(false)}
       />
     </SplashView>
   );
